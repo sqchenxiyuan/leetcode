b3b8a9da4283c4b60a11bf93b0890da057c2dc56
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-03-26 20:46:57 +0800

    over | 29

--- [20-29]/29.divide-two-integers/29.divide-two-integers.js
+++ [20-29]/29.divide-two-integers/29.divide-two-integers.js
@@ -1,3 +1,47 @@
+/*
+ * @lc app=leetcode id=29 lang=javascript
+ *
+ * [29] Divide Two Integers
+ *
+ * https://leetcode.com/problems/divide-two-integers/description/
+ *
+ * algorithms
+ * Medium (16.13%)
+ * Total Accepted:    185.4K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '10\n3'
+ *
+ * Given two integers dividend and divisor, divide two integers without using
+ * multiplication, division and mod operator.
+ * 
+ * Return the quotient after dividing dividend by divisor.
+ * 
+ * The integer division should truncate toward zero.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: dividend = 10, divisor = 3
+ * Output: 3
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: dividend = 7, divisor = -3
+ * Output: -2
+ * 
+ * Note:
+ * 
+ * 
+ * Both dividend and divisor will be 32-bit signed integers.
+ * The divisor will never be 0.
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
+ * of this problem, assume that your function returns 2^31 − 1 when the
+ * division result overflows.
+ * 
+ * 
+ */
 /**
  * @param {number} dividend
  * @param {number} divisor
@@ -53,10 +97,3 @@ let divide = function(dividend, divisor) {
     
     return number
 }
-
-console.log(Math.pow(2, 31))
-console.log(divide(10, 3)) //3
-console.log(divide(7, -3)) //-2
-console.log(divide(1, 1)) //-2
-console.log(divide(2147483648, 1)) //-2
-console.log(divide(-2147483648, 1)) //-2
