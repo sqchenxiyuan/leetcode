53815ad9fd7bf6314a9db098a3b9a1b2060b02e7
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-04-05 10:35:26 +0800

    over | 18

--- [1-99]/[10-19]/18 4Sum/18.4-sum.js
+++ [1-99]/[10-19]/18 4Sum/18.4-sum.js
@@ -1,3 +1,38 @@
+/*
+ * @lc app=leetcode id=18 lang=javascript
+ *
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (29.90%)
+ * Total Accepted:    221.1K
+ * Total Submissions: 735.8K
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array nums of n integers and an integer target, are there elements
+ * a, b, c, and d in nums such that a + b + c + d = target? Find all unique
+ * quadruplets in the array which gives the sum of target.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate quadruplets.
+ * 
+ * Example:
+ * 
+ * 
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1,  0, 0, 1],
+ * ⁠ [-2, -1, 1, 2],
+ * ⁠ [-2,  0, 0, 2]
+ * ]
+ * 
+ * 
+ */
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -37,7 +72,3 @@ let fourSum = function(nums, target) {
     return result
 }
 
-// console.log(JSON.stringify(fourSum([1, 0, -1, 0, -2, 2], 0)))
-// console.log(JSON.stringify(fourSum([1, 0, -1, 0, -2, 2], 1)))
-// console.log(JSON.stringify(fourSum([1, 0, -1, 0, -2, 2], 2)))
-// console.log(JSON.stringify(fourSum([1, 1, 0, 0, -1, 0, -1, 0, -2, 2], 2)))
