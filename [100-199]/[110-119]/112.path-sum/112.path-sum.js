71dc510ab6af3c72d871207c7d9f63bbb99f2aff
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-06-10 10:16:51 +0800

    over | 112

---
+++ [100-199]/[110-119]/112.path-sum/112.path-sum.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=112 lang=javascript
+ *
+ * [112] Path Sum
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+let hasPathSum = function(root, sum) {
+    if (root === null) return false
+    if (root.left === null && root.right === null) return root.val === sum
+    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+}
