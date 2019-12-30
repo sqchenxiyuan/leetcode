4d1ff0e473e8c46fd9598961cb1afbc4663b72c0
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-12-30 16:50:55 +0800

    over | 429

---
+++ [400-499]/[420-429]/429.n-ary-tree-level-order-traversal/429.n-ary-tree-level-order-traversal.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=429 lang=java
+ *
+ * [429] N-ary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        
+    }
+}
+// @lc code=end
+
