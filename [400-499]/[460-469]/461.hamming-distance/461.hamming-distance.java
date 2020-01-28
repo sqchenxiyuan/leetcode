ec5614df9755b1b7451c2d6d3799be006bccb225
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-01-28 09:11:34 +0800

    over | 461

---
+++ [400-499]/[460-469]/461.hamming-distance/461.hamming-distance.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=461 lang=java
+ *
+ * [461] Hamming Distance
+ */
+
+// @lc code=start
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        while(x > 0 || y > 0){
+            if((x & 1) != (y & 1)){
+                count++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return count;
+    }
+}
+// @lc code=end
+
