2f3ac15bed6376cf8a0f17c35cc55fc1168230af
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-02-21 08:59:30 +0800

    over | 492

---
+++ [400-499]/[490-499]/492.construct-the-rectangle/492.construct-the-rectangle.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=492 lang=java
+ *
+ * [492] Construct the Rectangle
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] constructRectangle(int area) {
+        for(int i = (int)Math.sqrt(area); i >= 1; i--){
+            if(area % i == 0){
+                return new int[]{area / i, i};
+            }
+        }
+        return new int[]{area, 1};
+    }
+}
+// @lc code=end
+
