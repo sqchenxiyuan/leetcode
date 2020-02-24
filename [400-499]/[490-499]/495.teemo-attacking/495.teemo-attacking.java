a5c1d75fc57f5eb31ad1ec628124ac0f09a29554
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-02-24 09:01:29 +0800

    over | 495

---
+++ [400-499]/[490-499]/495.teemo-attacking/495.teemo-attacking.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=495 lang=java
+ *
+ * [495] Teemo Attacking
+ */
+
+// @lc code=start
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        
+    }
+}
+// @lc code=end
+
