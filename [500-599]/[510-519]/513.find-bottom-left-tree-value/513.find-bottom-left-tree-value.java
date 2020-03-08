fbfb5416732b744ec877e77e76b36544524349c8
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-03-08 09:19:29 +0800

    over | 513

---
+++ [500-599]/[510-519]/513.find-bottom-left-tree-value/513.find-bottom-left-tree-value.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=513 lang=java
+ *
+ * [513] Find Bottom Left Tree Value
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        
+    }
+}
+// @lc code=end
+
