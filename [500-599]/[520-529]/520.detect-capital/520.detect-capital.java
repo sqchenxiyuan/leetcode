379ebb40a07285b896c6118a76481541ef75e6a4
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-03-15 16:43:59 +0800

    over | 520

---
+++ [500-599]/[520-529]/520.detect-capital/520.detect-capital.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=520 lang=java
+ *
+ * [520] Detect Capital
+ */
+
+// @lc code=start
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        
+    }
+}
+// @lc code=end
+
