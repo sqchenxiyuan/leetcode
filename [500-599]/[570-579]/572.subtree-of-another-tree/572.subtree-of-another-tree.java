b880852c85c674800a17ac00e728bb4c268c1225
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-04-22 21:49:18 +0800

    over | 572

--- [500-599]/[570-579]/572.subtree-of-another-tree/572.subtree-of-another-tree.java
+++ [500-599]/[570-579]/572.subtree-of-another-tree/572.subtree-of-another-tree.java
@@ -14,9 +14,18 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+public class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null) return sametree(s, t);
+        return sametree(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
 
+    public boolean sametree(TreeNode s, TreeNode t) {
+        if(s == null && t == null) return true;
+        else if(s == null || t == null) return false;
+        else {
+            return s.val == t.val && sametree(s.left, t.left) && sametree(s.right, t.right);
+        }
     }
 }
 // @lc code=end
