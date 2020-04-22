01dd33ee9365d45d1e21148c2efbba172dbaac8a
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-04-22 20:33:02 +0800

    over | 572

---
+++ [500-599]/[570-579]/572.subtree-of-another-tree/572.subtree-of-another-tree.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=java
+ *
+ * [572] 另一个树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+    }
+}
+// @lc code=end
+
