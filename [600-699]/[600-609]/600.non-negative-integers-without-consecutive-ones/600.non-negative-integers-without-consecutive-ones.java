e770d70fcf2b15d9bc7a6387db029fd67373f32a
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-05-07 09:27:05 +0800

    over | 600

---
+++ [600-699]/[600-609]/600.non-negative-integers-without-consecutive-ones/600.non-negative-integers-without-consecutive-ones.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=600 lang=java
+ *
+ * [600] 不含连续1的非负整数
+ */
+
+// @lc code=start
+class Solution {
+    public int findIntegers(int num) {
+
+    }
+}
+// @lc code=end
+
