437d4f8d09f5a2bf3083678b01829914b72a20f0
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-05-18 09:20:20 +0800

    over | 629

---
+++ [600-699]/[620-629]/629.k-inverse-pairs-array/629.k-inverse-pairs-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=629 lang=java
+ *
+ * [629] K个逆序对数组
+ */
+
+// @lc code=start
+class Solution {
+    public int kInversePairs(int n, int k) {
+
+    }
+}
+// @lc code=end
+
