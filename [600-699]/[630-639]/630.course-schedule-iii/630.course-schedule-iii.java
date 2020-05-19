2288864a5cad9ca01b4d59fde89670e3ae3538c1
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-05-19 14:32:23 +0800

    over | 630

--- [600-699]/[630-639]/630.course-schedule-iii/630.course-schedule-iii.java
+++ [600-699]/[630-639]/630.course-schedule-iii/630.course-schedule-iii.java
@@ -5,9 +5,26 @@
  */
 
 // @lc code=start
-class Solution {
-    public int scheduleCourse(int[][] courses) {
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
+public class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+
+        int time = 0;
+        for(int[] c : courses){
+            if(time + c[0] <= c[1]){
+                queue.add(c[0]);
+                time += c[0];
+            } else if(!queue.isEmpty() && queue.peek() > c[0]){
+                time += c[0] - queue.poll();
+                queue.add(c[0]);
+            }
+        }
+
+        return queue.size();
     }
 }
 // @lc code=end
