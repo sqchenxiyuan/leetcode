96b62e97406ba04c72629f7ddf9a8ffe70a483f4
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-05-23 16:58:38 +0800

    over | 637

---
+++ [600-699]/[630-639]/637.average-of-levels-in-binary-tree/637.average-of-levels-in-binary-tree.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=java
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
