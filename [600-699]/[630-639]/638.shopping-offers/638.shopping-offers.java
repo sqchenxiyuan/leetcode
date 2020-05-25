40204c1b33cdd0a9acc85c0cc53e43a094ef573f
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-05-25 08:02:43 +0800

    over | 638

---
+++ [600-699]/[630-639]/638.shopping-offers/638.shopping-offers.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=638 lang=java
+ *
+ * [638] 大礼包
+ */
+
+// @lc code=start
+class Solution {
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+
+    }
+}
+// @lc code=end
+
