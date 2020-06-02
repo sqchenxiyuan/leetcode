e0c1ee9ee3f3bf113b03092859795efcd4cae5d8
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-02 10:12:38 +0800

    over | 646

---
+++ [600-699]/[640-649]/646.maximum-length-of-pair-chain/646.maximum-length-of-pair-chain.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=646 lang=java
+ *
+ * [646] 最长数对链
+ */
+
+// @lc code=start
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+
+    }
+}
+// @lc code=end
+
