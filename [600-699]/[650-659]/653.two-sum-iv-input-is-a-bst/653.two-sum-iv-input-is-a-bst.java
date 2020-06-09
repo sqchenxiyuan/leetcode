520a9f65d551f56b2ccc42c86aadce5a1d906d82
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-09 10:26:00 +0800

    over | 653

---
+++ [600-699]/[650-659]/653.two-sum-iv-input-is-a-bst/653.two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=653 lang=java
+ *
+ * [653] 两数之和 IV - 输入 BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+
+    }
+}
+// @lc code=end
+
