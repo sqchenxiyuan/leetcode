f53b31c50541c1d972b843b84e203a3e372844ed
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-09 10:28:57 +0800

    over | 653

--- [600-699]/[650-659]/653.two-sum-iv-input-is-a-bst/653.two-sum-iv-input-is-a-bst.java
+++ [600-699]/[650-659]/653.two-sum-iv-input-is-a-bst/653.two-sum-iv-input-is-a-bst.java
@@ -14,9 +14,22 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    public boolean findTarget(TreeNode root, int k) {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Solution {
+    private Set<Integer> set = new HashSet<>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        return help(root, k);
+    }
+
+    private boolean help(TreeNode root, int k){
+        if(root == null) return false;
+        if(set.contains(k - root.val)) return true;
+        set.add(root.val);
+
+        return help(root.left, k) || help(root.right, k);
     }
 }
 // @lc code=end
