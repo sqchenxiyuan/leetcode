5e66500aea8887c21e984c54f1db1c0780af8d53
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-10 10:17:20 +0800

    over | 654

---
+++ [600-699]/[650-659]/654.maximum-binary-tree/654.maximum-binary-tree.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=java
+ *
+ * [654] 最大二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+
+    }
+}
+// @lc code=end
+
