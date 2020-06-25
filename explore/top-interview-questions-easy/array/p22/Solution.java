092638130221880354648ceb756059f651c3a260
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-25 09:27:00 +0800

    over | explore/top-interview-questions-easy/array/p22

---
+++ explore/top-interview-questions-easy/array/p22/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for(int i = 1; i < prices.length; i++){
+            sum += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return sum;
+    }
+}
