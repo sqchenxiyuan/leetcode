dbb2c9b56931be6aed8c7aab2d8e05ab0c26a027
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-06-29 10:11:30 +0800

    over | explore/top-interview-questions-easy/array/p24

---
+++ explore/top-interview-questions-easy/array/p24/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int num : nums){
+            if(set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
