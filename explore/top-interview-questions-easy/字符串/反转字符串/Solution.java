346636826cac052c6628089932e47524295457b3
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-07-07 10:58:01 +0800

    over | 初级算法-反转字符串

---
+++ explore/top-interview-questions-easy/字符串/反转字符串/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public void reverseString(char[] s) {
+        int len = s.length;
+        for(int i = 0; i < len / 2; i++){
+            char c = s[i];
+            s[i] = s[len - 1 - i];
+            s[len - 1 - i] = c;
+        }
+    }
+}
