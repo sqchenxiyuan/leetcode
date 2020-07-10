045d12afec471bfff9824ebeec6ed43f3acbe9f9
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-07-10 11:23:26 +0800

    over | 初级算法-有效的字母异位词

---
+++ explore/top-interview-questions-easy/字符串/有效的字母异位词/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] arr = new int[26];
+        for(char c : s.toCharArray()){
+            arr[c - 'a']++;
+        }
+
+        for(char c : t.toCharArray()){
+            arr[c - 'a']--;
+        }
+
+        for(int x : arr){
+            if(x != 0) return false;
+        }
+        return true;
+    }
+}
