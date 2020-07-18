2fc6895af920636543026d00b93e2437189813c2
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-07-18 14:50:26 +0800

    over | 初级算法-反转链表

---
+++ explore/top-interview-questions-easy/链表/反转链表/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+}
