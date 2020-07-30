bf70cebaf17a780c0bbff53d46ed5d743ec177a6
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-07-30 09:25:52 +0800

    over | 初级算法-买卖股票的最佳时机

---
+++ explore/top-interview-questions-easy/动态规划/买卖股票的最佳时机/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < prices.length; i++){
+            min = Math.min(prices[i], min);
+            max = Math.max(prices[i] - min, max);
+        }
+        return max;
+    }
+}
