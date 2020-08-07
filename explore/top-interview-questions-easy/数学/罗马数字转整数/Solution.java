d3174b7eac4fa23f9b8efd6bd3dee0d103229bd0
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-08-07 19:47:46 +0800

    over | 罗马数字转整数

---
+++ explore/top-interview-questions-easy/数学/罗马数字转整数/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        char[] chars = s.toCharArray();
+        int num = 0;
+        for(int i = 0; i < chars.length; i++){
+            num += map.get(chars[i]);
+            if(i > 0 && map.get(chars[i]) > map.get(chars[i - 1])){
+                num -= 2 *  map.get(chars[i - 1]);
+            }
+        }
+        return num;
+    }
+}
