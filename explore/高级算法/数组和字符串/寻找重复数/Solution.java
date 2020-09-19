edfe864d8b2c185f1fc3ee6983375027af5ebe06
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2020-09-19 22:28:01 +0800

    over | 寻找重复数

---
+++ explore/高级算法/数组和字符串/寻找重复数/Solution.java
@@ -0,0 +1,18 @@
+
+//双指针
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(fast != slow){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while(fast != slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
+}
